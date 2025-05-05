22af58a9655bd4594fba3aa5784373d4f09b1b8b
等风几许JX <92464802+cupide007@users.noreply.github.com>
2025-05-05 20:53:30 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -5,10 +5,10 @@
 _constant() {
     script_version="v2024-04-25"
     old_IFS="$IFS"
-    work_dir="./sp-github-i-abc"
+    work_dir="./sp-github-cupide007"
     node_set=""
-    node_set_1="https://raw.githubusercontent.com/i-abc/speedtest/node/all-node.txt"
-    node_set_2="https://github.cupide.cn/https://raw.githubusercontent.com/i-abc/speedtest/node/all-node-mirror.txt"
+    node_set_1="https://raw.githubusercontent.com/cupide007/speedtest/node/all-node.txt"
+    node_set_2="https://github.cupide.cn/https://raw.githubusercontent.com/cupide007/speedtest/node/all-node-mirror.txt"
 
     # url_1为官方源，url_2为镜像源，皆会进行SHA-256检测
 
@@ -21,7 +21,7 @@ _constant() {
     speedtest_cli_tar_armel_sha256="629a455a2879224bd0dbd4b36d8c721dda540717937e4660b4d2c966029466bf"
     speedtest_cli_tar_url=""
     speedtest_cli_tar_url_1="https://install.speedtest.net/app/cli/ookla-speedtest-${speedtest_cli_version}-linux-${speedtest_cli_arch}.tgz"
-    speedtest_cli_tar_url_2="https://github.cupide.cn/https://raw.githubusercontent.com/i-abc/speedtest/asset/speedtest-cli/v${speedtest_cli_version}/ookla-speedtest-${speedtest_cli_version}-linux-${speedtest_cli_arch}.tgz"
+    speedtest_cli_tar_url_2="https://github.cupide.cn/https://raw.githubusercontent.com/cupide007/speedtest/asset/speedtest-cli/v${speedtest_cli_version}/ookla-speedtest-${speedtest_cli_version}-linux-${speedtest_cli_arch}.tgz"
 
     # bim-core，https://github.com/veoco/bim-core
     bim_core_version="0.17.0"
@@ -79,7 +79,7 @@ _print_banner_1() {
     echo "------------------------ 多功能 自更新 测速脚本 ------------------------"
     echo -e " Version               : ${green}${script_version}${endc}"
     echo -e " Usage                 : ${yellow}bash <(curl -sL bash.icu/speedtest)${endc}"
-    echo -e " GitHub                : ${green}https://github.com/i-abc/speedtest${endc}"
+    echo -e " GitHub                : ${green}https://github.com/cupide007/speedtest${endc}"
     printf "%-72s\n" "-" | sed 's)\s)-)g'
 }
 
